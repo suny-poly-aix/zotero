d60c07ee23099a5c5d1c324290b9f7739d8e226b
SUNY POLY AIX <sunypolyaix@gmail.com>
2025-06-20 16:58:16 -0400

    Update wiki_sync.py

--- wiki_sync.py
+++ wiki_sync.py
@@ -336,7 +336,6 @@ def main():
     # Configuration
     wiki_urls = [
         "https://en.wikiversity.org/wiki/AIXworkbench/Papers/Building-the-Workbench", "https://en.wikiversity.org/wiki/AIXworkbench/Papers/Building-the-Workbench/Open-WebUI"
-        
     ]
     
     print("Starting automated Zotero-Wiki sync...")
